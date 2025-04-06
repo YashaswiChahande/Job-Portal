db48e8023d88a1b468e3914eed6b66ed47f6c439
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-04-06 09:53:34 +0530

    Landing fin Onboarding init

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import MyJobs from './pages/my-jobs'
 import SavedJobs from './pages/saved-job'
 import JobPage from './pages/job'
 import { ThemeProvider } from './components/theme-provider'
+import ProtectedRoute from './components/protected-route'
 
 
 const router = createBrowserRouter([
@@ -23,43 +24,49 @@ const router = createBrowserRouter([
       {
         path: "/onboarding",
         element: (
-          
+          <ProtectedRoute>
             <Onboarding />
+          </ProtectedRoute>
         ),
       },
       {
         path: "/jobs",
         element: (
-          
+          <ProtectedRoute>
             <JobListing />
+          </ProtectedRoute>
         ),
       },
       {
         path: "/post-job",
         element: (
-          
+          <ProtectedRoute>
             <PostJob />
+          </ProtectedRoute>
         ),
       },
       {
         path: "/my-jobs",
         element: (
-          
+          <ProtectedRoute>
             <MyJobs />
+          </ProtectedRoute>
         ),
       },
       {
         path: "/saved-jobs",
         element: (
-          
+          <ProtectedRoute>
             <SavedJobs />
+          </ProtectedRoute>
         ),
       },
       {
         path: "/job/:id",
         element: (
-          
+          <ProtectedRoute>
             <JobPage />
+          </ProtectedRoute>
         ),
       },
     ],
--- src/pages/saved-job.jsx
+++ src/pages/saved-job.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SavedJobs = () => {
   return (
     <div>
-        
+        SavedJobs
     </div>
   )
 }
